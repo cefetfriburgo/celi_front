7f4f7507cd2b013fca9f7ce6815b8c2f2087a904
JoaoLuizRSilva <silvajoaoluiz360@gmail.com>
2025-01-31 16:44:22 -0300

    Update atividade_inscricao.js

--- Scripts/services/atividade/atividade_inscricao.js
+++ Scripts/services/atividade/atividade_inscricao.js
@@ -3,8 +3,8 @@ export async function atividadeInscricao(idAtividade, idParticipante){
         window.location.assign('https://celi.cefet-rj.br/coordenacao/atividades/sign_in.html');
     }else{
         if(confirm("Tem certezar que deseja se inscrever?")){
-            alert(idAtividade);
-            alert(idParticipante);
+            //alert(idAtividade);
+            //alert(idParticipante);
 
             await fetch(`https://celi.cefet-rj.br/coordenacao/api/participante`, {
                 method: 'POST',
