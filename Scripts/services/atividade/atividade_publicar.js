f314e854897e3831ab199960acb022b389694cf9
JoaoLuizRSilva <silvajoaoluiz360@gmail.com>
2025-01-31 16:00:21 -0300

    Update atividade_publicar.js

--- Scripts/services/atividade/atividade_publicar.js
+++ Scripts/services/atividade/atividade_publicar.js
@@ -1,14 +1,12 @@
 export async function atividadePublicar(id){
     if(confirm("Tem certeza que deseja Publicar?")){
-        alert(`Atividade ${id} publicada`);
-
         await fetch(`https://celi.cefet-rj.br/coordenacao/api/atividade/${id}/status/`, {
             method: 'PUT',
             headers:{
                 'Authorization': `Bearer ${localStorage.getItem('chave')}`
             }
         })
-        alert(`Atividade ${id} publicada`);
+        alert(`A Atividade foi publicada`);
 
         window.location.assign('https://celi.cefet-rj.br/coordenacao/atividades/adm.html');
     }
